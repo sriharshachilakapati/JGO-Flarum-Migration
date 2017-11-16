d3a04e3a4c50aee7305f96382fa057fa49577a5e
Sri Harsha Chilakapati <sriharshachilakapati@gmail.com>
2017-11-16 09:53:42 +0530

    Error message, progress reports and member groups

--- migrate.php
+++ migrate.php
@@ -29,8 +29,14 @@ function migrateCategories($jgo, $fla)
 
     $insert = $fla->prepare('INSERT INTO `jgoforumstags` (name, slug, description, color, position) VALUES (?, ?, ?, ?, ?)');
 
+    $total = $jgo->query('SELECT COUNT(*) FROM `jgoforums_categories`')->fetchColumn();
+    $done = 0;
+
     while ($row = $stmt->fetch())
     {
+        $done++;
+        echo "Migrating categories: " . $done . "/" . $total . " (" . ((int) ($done / $total * 100)) . "%)\r";
+
         $data = array(
             preg_replace('(\\&amp;)', '&', $row->name),
             slugify($row->name),
@@ -41,6 +47,8 @@ function migrateCategories($jgo, $fla)
 
         $insert->execute($data);
     }
+
+    echo "\n";
 }
 
 function migrateBoards($jgo, $fla)
@@ -56,8 +64,14 @@ SQL;
 
     $insert = $fla->prepare('INSERT INTO `jgoforumstags` (name, slug, description, color, position, parent_id) VALUES (?, ?, ?, ?, ?, ?)');
 
+    $total = $jgo->query('SELECT COUNT(*) FROM `jgoforums_boards`')->fetchColumn();
+    $done = 0;
+
     while ($row = $stmt->fetch())
     {
+        $done++;
+        echo "Migrating boards: " . $done . "/" . $total . " (" . ((int) ($done / $total * 100)) . "%)\r";
+
         if ($row->childLevel === "0")
         {
             $stmt2 = $fla->query('SELECT * FROM `jgoforumstags` WHERE slug=\'' . slugify($row->cname) . '\'');
@@ -90,35 +104,66 @@ SQL;
             $insert->execute($data);
         }
     }
+
+    echo "\n";
 }
 
 function migrateUsers($jgo, $fla)
 {
     $fla->exec('DELETE FROM `jgoforumsusers` WHERE id != 1');
     $fla->exec('ALTER TABLE `jgoforumsusers` AUTO_INCREMENT = 2');
+    $fla->exec('DELETE FROM `jgoforumsusers_groups` WHERE user_id != 1');
+    $fla->exec('ALTER TABLE `jgoforumsusers_groups` AUTO_INCREMENT = 2');
 
     $sql = <<<SQL
-        SELECT ID_MEMBER, memberName, emailAddress, dateRegistered, lastLogin, personalText, is_activated
+        SELECT ID_MEMBER, memberName, emailAddress, dateRegistered, lastLogin, personalText, is_activated, ID_GROUP
         FROM `jgoforums_members` WHERE lastLogin != 0 ORDER BY ID_MEMBER ASC
 SQL;
 
     $stmt = $jgo->query($sql);
     $stmt->setFetchMode(PDO::FETCH_OBJ);
 
+    $total = $jgo->query('SELECT COUNT(*) FROM `jgoforums_members` WHERE lastLogin != 0')->fetchColumn();
+    $done = 0;
+
     $sql = <<<SQL
         INSERT INTO `jgoforumsusers` (username, email, is_activated, password, bio, join_time, last_seen_time)
         VALUES (
             ?, ?, ?, '', ?, ?, ?
-        )
+        );
 SQL;
 
     $insert = $fla->prepare($sql);
+    $insert2 = $fla->prepare('INSERT INTO `jgoforumsusers_groups` (user_id, group_id) VALUES (?, ?)');
+
+    $groupMap = array(
+        0 => 3,
+        1 => 1,
+        3 => 4,
+        2 => 3,
+        4 => 3,
+        5 => 3,
+        6 => 3,
+        7 => 3,
+        8 => 3,
+        9 => 3,
+        10 => 3,
+        11 => 3,
+        12 => 3,
+        13 => 4,
+        14 => 4,
+        15 => 1,
+        16 => 1
+    );
 
     while ($row = $stmt->fetch())
     {
+        $done++;
+        echo "Migrating users: " . $done . "/" . $total . " (" . ((int) ($done / $total * 100)) . "%)\r";
+
         $data = array(
             $row->memberName,
-            $row->emailAddress === "" ? slugify($row->memberName) . "-" . $row->ID_MEMBER . "@example.com" : $row->emailAddress,
+            $row->emailAddress === "" ? "email" . $row->ID_MEMBER . "@example.com" : $row->emailAddress,
             $row->is_activated !== "0" ? 1 : 0,
             $row->personalText,
             date(DateTime::ATOM, $row->dateRegistered),
@@ -128,6 +173,22 @@ SQL;
         try
         {
             $insert->execute($data);
+
+            $data = array(
+                $fla->lastInsertId(),
+                $groupMap[(int) $row->ID_GROUP]
+            );
+
+            try
+            {
+                $insert2->execute($data);
+            }
+            catch (Exception $e)
+            {
+                echo "Error while updating members for the following user:\n";
+                var_dump($data);
+                echo "The message was: " . $e->getMessage() . "\n";
+            }
         }
         catch (Exception $e)
         {
@@ -136,6 +197,8 @@ SQL;
             echo "The message was: " . $e->getMessage() . "\n";
         }
     }
+
+    echo "\n";
 }
 
 function slugify($text)
